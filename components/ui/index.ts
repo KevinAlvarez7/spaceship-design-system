8b756a09df988ede4c6a13c3374c001188271873
Kev Alvarez <kevinalvarez373@gmail.com>
2026-03-01 22:25:18 +0800

    feat: add Input, Card, Badge DS components

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,2 +1,10 @@
 export { Button, buttonVariants } from './button';
 export type { ButtonProps } from './button';
+
+export { Input, inputVariants } from './input';
+export type { InputProps } from './input';
+
+export { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from './card';
+
+export { Badge, badgeVariants } from './badge';
+export type { BadgeProps } from './badge';
